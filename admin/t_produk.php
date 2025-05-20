3e465a13cf08ecdd6608322d0f564d73ffc447fc
alifna <praptamakukuh@gmail.com>
2025-05-20 21:59:24 +0700

    membuat file pengguna

--- admin/t_produk.php
+++ admin/t_produk.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION["login"])) {
 }
 
 // Cek apakah status tersedia dan pastikan user adalah admin
-if (!isset($_SESSION["status"]) || $_SESSION["status"] !== "admin") {
+if (!isset($_SESSION["sts"]) || $_SESSION["sts"] !== "admin") {
     echo "<script>
     alert('Akses ditolak! Halaman ini hanya untuk Admin.');
     window.location.href='login.php';
