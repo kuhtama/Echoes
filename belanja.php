dc00af9f0cc4ec540227a0ff644e969ab4d6a9d5
kuhtama <kukuh11122004@gmail.com>
2025-06-12 06:19:14 +0700

    memperbarui file all

--- belanja.php
+++ belanja.php
@@ -331,6 +331,53 @@
                                     </h3><!-- End .widget-title -->
 
                                     <div class="collapse show" id="widget-2">
+										<div class="widget-body">
+											<div class="filter-items">
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="size-1">
+														<label class="custom-control-label" for="size-1">XS</label>
+													</div><!-- End .custom-checkbox -->
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="size-2">
+														<label class="custom-control-label" for="size-2">S</label>
+													</div><!-- End .custom-checkbox -->
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" checked id="size-3">
+														<label class="custom-control-label" for="size-3">M</label>
+													</div><!-- End .custom-checkbox -->
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" checked id="size-4">
+														<label class="custom-control-label" for="size-4">L</label>
+													</div><!-- End .custom-checkbox -->
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="size-5">
+														<label class="custom-control-label" for="size-5">XL</label>
+													</div><!-- End .custom-checkbox -->
+												</div><!-- End .filter-item -->
+
+												<div class="filter-item">
+													<div class="custom-control custom-checkbox">
+														<input type="checkbox" class="custom-control-input" id="size-6">
+														<label class="custom-control-label" for="size-6">XXL</label>
+													</div><!-- End .custom-checkbox -->
+												</div><!-- End .filter-item -->
+											</div><!-- End .filter-items -->
+										</div><!-- End .widget-body -->
+
+                                    <div class="collapse show" id="widget-2">
                                         <div class="widget-body">
                                             <div class="filter-items">
                                                 <?php
